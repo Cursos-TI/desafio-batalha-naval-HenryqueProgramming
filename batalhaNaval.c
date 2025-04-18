d12e07f583466d536cbddc8223fc05c70c9a6bf3
HenryqueProgramming <chenryquenewman@gmail.com>
2025-04-18 21:38:48 +0000

    Posicionamento dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -18,3 +18,14 @@ int main(){
 
     int navio_horizontal[3] = {3, 3, 3};
     int navio_vertical[3] = {3, 3, 3};
+
+    
+    // Posicionamento dos navios
+
+    tabuleiro[1][3] = 3;
+    tabuleiro[1][4] = 3;
+    tabuleiro[1][5] = 3;
+
+    tabuleiro[4][6] = 3;
+    tabuleiro[5][6] = 3;
+    tabuleiro[6][6] = 3;
