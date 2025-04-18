ebb42860e8950847a9450747e5586d908080d51e
HenryqueProgramming <chenryquenewman@gmail.com>
2025-04-18 21:38:00 +0000

    Tabuleiro e Representação dos Navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,3 +6,15 @@ Arrays unidimensionais e bidimensionais
 
 */
 
+include <stdio.h>
+
+int main(){
+
+    // Tabuleiro - 10X10
+
+    int tabuleiro[10][10] = {0};
+
+    // Representação dos navios
+
+    int navio_horizontal[3] = {3, 3, 3};
+    int navio_vertical[3] = {3, 3, 3};
